1489dd13c444019059374baf26c6cf7046fa2fde
alexserg1998 <alexpon1998@yandex.ru>
2022-05-29 21:13:16 +0300

    add file

--- src/models/sbert.py
+++ src/models/sbert.py
@@ -47,6 +47,8 @@ class SBERTModel(pl.LightningModule):
         output = subtracted_dense
         loss = 0
         if labels is not None:
+            print(labels.shape)
+            print(labels)
             loss = self.criterion(output, labels)
         return loss, output
 
